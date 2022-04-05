c1f252d747026191df6ce16a5e5f6e027dbed51e
psi <psi.palmate@gmail.com>
2022-04-05 16:48:11 +0530

    model edit with customer id added

--- src/BsModal.php
+++ src/BsModal.php
@@ -29,9 +29,11 @@ public function openModal($component, $componentAttributes = [], $modalAttribute
         $this->component = $component;
         $this->componentAttributes = $componentAttributes;
         $this->dispatchBrowserEvent('openModalInBrowser');
-        if (isset($componentAttributes['id'])) {
+        if(isset($componentAttributes['customer_id']) && isset($componentAttributes['id'])) {
+            $this > $this->emitTo($component, 'editFormWithCustomerID', $componentAttributes['id'], $componentAttributes['customer_id']);
+        }elseif(isset($componentAttributes['id'])) {
             $this > $this->emitTo($component, 'editForm', $componentAttributes['id']);
-        } elseif(isset($componentAttributes['customer_id'])) {
+        }elseif(isset($componentAttributes['customer_id'])) {
             $this > $this->emitTo($component, 'addFormWithCustomerID', $componentAttributes['customer_id']);
         }else {
             $this > $this->emitTo($component, 'addForm');
